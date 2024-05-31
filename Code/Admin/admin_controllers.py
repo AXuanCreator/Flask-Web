a0816d0848422b0af33c946f6d5c387c1cc15e96
YuXeng <1917912744@qq.com>
2024-05-31 23:44:54 +0800

    admin model coding

--- Code/Admin/admin_controllers.py
+++ Code/Admin/admin_controllers.py
@@ -1,19 +1,30 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, request
+from admin_services import AdminService
+from Code.Utils.response import ResponseCode, Response
 
 admin_bp = Blueprint('admin', __name__)
 
 
 @admin_bp.route('/login', methods=['POST'])
 def login():
-    """管理员登录"""
-    pass
-
+    username = request.get_json().get('username')
+    password = request.get_json().get('password')
+    service_code = AdminService.login(username=username, password=password)
+    if service_code == 1:
+        admin = AdminService.get_admin_by_username(username)
+        return Response.response(ResponseCode.LOGIN_SUCCESS, '登录成功', admin.id)
+    if service_code == 0:
+        return Response.response(ResponseCode.WRONG_PASSWORD, '密码错误', 0)
+    if service_code == -1:
+        return Response.response(ResponseCode.ACCOUNT_NOT_EXIST, '管理员不存在', 0)
 
 @admin_bp.route('/<int:id>', methods=['GET'])
-def query_admin_info():
+def query_admin_info(id):
     """获取管理员的基本信息"""
-    pass
-
+    admin = AdminService.get_admin_by_id(id)
+    if admin:
+        return Response.response(ResponseCode.SUCCESS, '查询成功', admin)
+    return Response.response(ResponseCode.ACCOUNT_NOT_EXIST, '用户不存在', 0)
 
 @admin_bp.route('/<int:id>', methods=['PUT'])
 def modify_admin_info():
--- Code/Admin/admin_services.py
+++ Code/Admin/admin_services.py
@@ -1,13 +1,52 @@
 #!/user/bin/env python3
 # -*- coding: utf-8 -*-
+from Code.Admin.admin import *
+
 
 class AdminService:
 
     @staticmethod
-    def get_admin_by_id(self):
-        pass
+    def get_admin_by_id(id):
+        Admin.query.get(id=id)
 
     @staticmethod
-    def update_admin(self, id, info):
-        pass
+    def get_admin_by_username(username):
+        Admin.query.filter_by(username)
+
+    @staticmethod
+    def login(username, password):
+        """
+        :param self:
+        :param username:
+        :param password:
+        :return: -1：管理员不存在；0：密码错误；1：登陆成功
+        """
+        login_admin = Admin.query.filter_by(username=username)
+        if login_admin:
+            if login_admin.password == password:
+                return 1
+            return 0
+        return -1
+
+
+    @staticmethod
+    def update_admin(id, info_json):
+        """
+        更新用户信息
+        :param self:
+        :param id:用户 id
+        :param info_json: 请求体 json
+        :return: -1：管理员不存在；1：修改成功
+        """
+        update_field = ['username', 'name', 'phone', 'email']
+        username, name, phone, email = (info_json.get(key) for key in update_field)
+        updated_admin = Admin.query.get(id=id)
+        if updated_admin:
+            updated_admin.username = username if username is not None else updated_admin.username
+            updated_admin.name = name if name is not None else updated_admin.name
+            updated_admin.phone = phone if phone is not None else updated_admin.phone
+            updated_admin.email = email if email is not None else updated_admin.email
+            db.session.commit()
+            return 1
+        return -1
 
